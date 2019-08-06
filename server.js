3e81b29467f0f7442f00108d1fd04fb4f1c4c720
Ryan Smith <ryan.l.smith4@gmail.com>
2019-08-06 01:26:41 -0700

    [DELETE] remove body-parser as it's replaced in express 4.16.0

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const express = require('express');
 
 const app = express();
 
-const bodyParser = require('body-parser');
-
 const exphbs = require('express-handlebars');
 
 require('dotenv').config();
 
 const weatherRoute = require('./routes/weather_routes');
+
 const moodRoute = require('./routes/mood');
+
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
 }));
 
 app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/api', weatherRoute, moodRoute);
 
 const { PORT } = process.env;
