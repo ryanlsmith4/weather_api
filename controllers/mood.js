c89cdea87b1e4475988438540ae10014aa3f5e89
Ryan Smith <ryan.l.smith4@gmail.com>
2019-08-06 01:32:33 -0700

    [ADD] added more functioanlity in the routes to interact with the user

--- controllers/mood.js
+++ controllers/mood.js
@@ -1,6 +1,14 @@
+// eslint-disable-next-line prefer-destructuring
+const LocalStorage = require('node-localstorage').LocalStorage;
+
+const localStorage = new LocalStorage('./scratch');
+
 const inputMood = (req, res) => {
   const { mood } = req.body;
-  const moodText = `your mood in this weather is ${mood}`;
+  const city = localStorage.getItem('city');
+  const weather = localStorage.getItem('weather')
+
+  const moodText = `When it's ${weather} in ${city}, you are usually ${mood}`;
   res.render('index', { current_mood: moodText });
 };
 
--- controllers/weather.js
+++ controllers/weather.js
@@ -3,6 +3,11 @@ const apiKey = process.env.WEATHER_API;
 
 const request = require('request');
 
+// eslint-disable-next-line prefer-destructuring
+const LocalStorage = require('node-localstorage').LocalStorage;
+
+const localStorage = new LocalStorage('./scratch');
+
 const getWeather = (req, res) => res.render('index');
 
 const recieveWeather = (req, res) => {
@@ -14,6 +19,10 @@ const recieveWeather = (req, res) => {
       res.render('index', { weather: null, error: 'Error, please try again' });
     } else {
       const weather = JSON.parse(body);
+
+      localStorage.setItem('weather', weather.weather[0].description);
+      localStorage.setItem('city', weather.name);
+
       if (weather.main === undefined) {
         res.render('index', { weather: null, error: 'Error, please try again' });
       } else {
